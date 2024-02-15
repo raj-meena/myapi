3a316609632490ceeaaea0c105a57047301cb8a9
raj-meena <raj.meena@hotmail.com>
2024-02-15 23:22:17 +0530

    gg

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const authRoutes = require("./router/login/login");
 //codebin
 const authCodebinRoutes = require("./router/codebin/login");
